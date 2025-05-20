be6abb33eff72de0f817e8e13e306cedb2f3ec23
loganoppenheime <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-05-20 23:46:21 +0000

    create follow_request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_233332) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_234550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_233332) do
     t.integer "author_id"
     t.text "body"
     t.integer "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
